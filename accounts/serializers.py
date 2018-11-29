5732352532eff49db3852bba86b5495d34ccb988
Jesus Anaya <jesus.anaya.dev@gmail.com>
2018-11-28 21:57:42 -0800

    Added endpoint to get user info.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
 class CreateUserSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=100)
     password = serializers.CharField(max_length=200)
+
+
+class CurrentUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username')
+        read_only_fields = ('id', 'username')
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken import views
-from accounts.views import CreateUser
+from accounts.views import CreateUser, CurrentUser
 
 
 urlpatterns = [
     path('api-token/', views.obtain_auth_token),
-    path('create-user/', CreateUser.as_view())
+    path('create-user/', CreateUser.as_view()),
+    path('current-user/', CurrentUser.as_view()),
 ]
